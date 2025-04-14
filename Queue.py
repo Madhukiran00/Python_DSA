ce8a05395b108ac960b13775e515b779a7612125
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-14 13:06:01 +0530

    Update Queue.py

---
+++ Queue.py
@@ -0,0 +1,38 @@
+#Queue
+
+class Queue():
+    def __init__(self):
+        self.queue=[]
+        self.index=0
+        self.numofitems=0
+        
+    def isempty(self):
+        return self.numofitems==0
+    
+    def enqueue(self,items):
+        self.queue.insert(self.index,items)
+        self.index+=1
+        self.numofitems+=1
+        return '{} added to queue'.format(items)
+    def dequeue(self):
+        if self.isempty()==True:
+            return 'Queue is empty'
+        else:
+            data=self.queue.pop(0)
+            self.index+=1
+            self.numofitems-=1
+            return '{} remove from queue'.format(data)
+        
+if __name__=='__main__':
+    q=Queue()
+    
+    print(q.enqueue(1))
+    print(q.enqueue(2))
+    print(q.enqueue(3))
+    print(q.enqueue(4))
+    print(q.enqueue(5))
+    print(q.enqueue(6))
+    print(q.enqueue(7))
+    
+    
+    
