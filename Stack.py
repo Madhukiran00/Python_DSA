8c896543125985f36cc8929af2d412433d8374b4
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-04-16 22:30:53 +0530

    Update Stack.py

--- Stack.py
+++ Stack.py
@@ -1,30 +1,36 @@
-# #Stack
-# class Stack():
-#     def __init__(self):
-#         self.stack=[]
-#         self.numofitems=0
+#Stack
+class Stack():
+    def __init__(self):
+        self.stack=[]
+        self.numofitems=0
         
-#     def isempty(self):
-#         return self.numofitems==0
+    def isempty(self):
+        return self.numofitems==0
     
-#     def push(self,item):
-#         self.stack.insert(self.numofitems,item)
-#         self.numofitems+=1
-#         return '{} pushed to stack'.format(item)
+    def push(self,item):
+        self.stack.insert(self.numofitems,item)
+        self.numofitems+=1
+        return '{} pushed to stack'.format(item)
     
-#     def pop(self):
-#         if self.isempty()==True:
-#             return 'Stack is empty'
-#         else:
-#             self.numofitems-=1
-#             data=self.stack.pop(self.numofitems)
+    def pop(self):
+        if self.isempty()==True:
+            return 'Stack is empty'
+        else:
+            self.numofitems-=1
+            data=self.stack.pop(self.numofitems)
             
-#             return '{} pop from stack'.format(data)
+            return '{} pop from stack'.format(data)
         
         
-# s=Stack()
-# 
+s=Stack()
+print(s.push(1))
+print(s.push(2))
+print(s.push(3)) 
 
+print("\n\n")
+
+print(s.pop())
+print(s.pop())
 
 
 
