9f7ce3349ea30dde4a2699c6d7585a883df70fba
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-01 19:59:02 +0530

    Create python.py

---
+++ 01-07-25/python.py
@@ -0,0 +1,11 @@
+lis=[[1,2,3],
+     [4,5,6],
+     [7,8,9]]
+
+def dig_ele(lis):
+    res=""
+    for i in range(len(lis)):
+        res+=str(lis[i][i])+" "))
+    
+    return res
+print(dig_ele(lis))
