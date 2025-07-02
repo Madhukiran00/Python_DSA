f12956ac06682d0c6a832bd7987020361302e694
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-02 19:34:36 +0530

    Create python.py

---
+++ 02-07-25/python.py
@@ -0,0 +1,52 @@
+
+# num=2518 #5182
+# list=["1534","2518","86","2001"]
+# new_list=[]
+
+# res=""
+# for j in range(len(list)):
+#     new_num=list[j]
+#     for i in range(len(new_num)):
+#         if int(new_num[i]) % 2 != 0:
+#             new_list.append( new_num[i:] + new_num[:i])
+#             break
+#         elif (len(new_num)-1)==i:
+#             new_list.append(new_num)
+            
+            
+# print(new_list)
+
+
+
+
+# s="abcde"
+# sub="ec"
+# k=0
+# for i in range(len(s)):
+#     if sub[k]==s[i]:
+#         k=k+1
+        
+# if k==len(sub):
+#     print("yes")
+# else:
+#     print("no")
+  
+
+# n = "21582940"
+# res = ""
+# for i in range(len(n)):
+#     if int(n[i]) % 2 != 0:
+#         res = n[i:]
+#         break
+# while int(res)%2==0:
+#     res=str(int(res)//10)
+# print(res)
+    
+    
+    
+    
+    
+    
+
+
+
