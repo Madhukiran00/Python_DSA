547185b22b5241d3adfebe17a94fb27fbe57faa4
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-03 15:13:50 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,8 +1,20 @@
-# n=253
-# string=str(n)
+n=2739
+# 
 
 # m500=int(string[-3:])//500
-# m1000=int(string[:-3])
+# m1000=n//1000
+# print("1000:",m1000)
+# temp1=n%1000 #539
+# m500=temp1//500
+# print("m500:",m500)
+# m100=(temp1%500)//200 #239
+# print("m100:",m100)
+# m50=((temp1%500)%200)//50
+# print("m50:",m50)
+# m10=((temp1%500)%200)//10
+# print("m10:",m10)
+# temp2=temp1%500
+
 # change=int(string[-3:])-500 #750
 # m100=change//100. #250 2
 # m50=(change-(m100*100))//50
@@ -10,12 +22,17 @@
 # print("1000s:{0} , 500s:{1} and 100s:{2} ,50s:{3}".format(m1000,m500,m100,m50))
 
 
-sec="3672"
-if len(sec)>=2:
-    
-    secounds=int(sec[-2:])%60
+# sec=38
 
+# hours=sec//3600
 
+# temp=sec%3600
+
+# minutes=temp//60
+
+# secounds=temp%60
+
+# print("hours:",hours,"minutes:",minutes,"secounds:",secounds)
 
 
 
