6996f920f5bb45f4a616b5093ba21a7e29ae791e
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-03 14:56:29 +0530

    Update python.py

--- python.py
+++ python.py
@@ -1,13 +1,21 @@
-n=253
-string=str(n)
+# n=253
+# string=str(n)
 
-m500=int(string[-3:])//500
-m1000=int(string[:-3])
-change=int(string[-3:])-500 #750
-m100=change//100. #250 2
-m50=(change-(m100*100))//50
-print(m50)
-print("1000s:{0} , 500s:{1} and 100s:{2} ,50s:{3}".format(m1000,m500,m100,m50))
+# m500=int(string[-3:])//500
+# m1000=int(string[:-3])
+# change=int(string[-3:])-500 #750
+# m100=change//100. #250 2
+# m50=(change-(m100*100))//50
+# print(m50)
+# print("1000s:{0} , 500s:{1} and 100s:{2} ,50s:{3}".format(m1000,m500,m100,m50))
+
+
+sec="3672"
+if len(sec)>=2:
+    
+    secounds=int(sec[-2:])%60
+
+
 
 
 
