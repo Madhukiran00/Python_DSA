4e07c55bb3a697f40911a2dd12b1cf9c7e864a4c
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-07 20:55:28 +0530

    Update python.py

--- 07-07-25/python.py
+++ 07-07-25/python.py
@@ -1,53 +1,12 @@
-# l=[[1,2,3],
-#    [4,5,6],
-#    [7,8,9]]
-
-# def spiralOrder(matrix):
-#     result = []
-#     if not matrix:
-#         return result
-
-#     top = 0
-#     bottom = len(matrix) - 1
-#     left = 0
-#     right = len(matrix[0]) - 1
-
-#     while top <= bottom and left <= right:
-#         # Left to Right
-#         for col in range(left, right + 1):
-#             result.append(matrix[top][col])
-#         top += 1
-
-#         # Top to Bottom
-#         for row in range(top, bottom + 1):
-#             result.append(matrix[row][right])
-#         right -= 1
-
-#         # Right to Left
-#         if top <= bottom:
-#             for col in range(right, left - 1, -1):
-#                 result.append(matrix[bottom][col])
-#             bottom -= 1
-
-#         # Bottom to Top
-#         if left <= right:
-#             for row in range(bottom, top - 1, -1):
-#                 result.append(matrix[row][left])
-#             left += 1
-
-#     return result
-
-
-
-# print(spiralOrder(l))
-
-
 
 l=[1,2,3,4,5]
+res=[]
+for i in range(len(l)-1,-1,-1):
+    res.append(l[i])
 
-
-
-
+print(res)
+#Output:[5,4,3,2,1]
+#-------------------------------------
 l=[1,2,3,4,5]
 i=0
 j=len(l)-1
@@ -58,7 +17,18 @@ while i<j:
     i+=1
     j-=1
 print(l)
-    
+#Output:[5,4,3,2,1]
+#-------------------------------------
+l=[1,2,3,4,5]
+i=0
+j=len(l)-1
+while i<j:
+    l[i],l[j]=l[j],l[i]
+    i+=1
+    j-=1
+print(l)
+#Output:[5,4,3,2,1]
+
 
 
 
