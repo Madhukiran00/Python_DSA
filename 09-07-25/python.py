4eb5041a0b12fda65c7d9094859488dc1334900e
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-09 22:20:12 +0530

    Update python.py

--- 09-07-25/python.py
+++ 09-07-25/python.py
@@ -1,3 +1,41 @@
+
+
+
+
+
+
+# Rotate Array
+arr=[4,5,6,7,8,1,2,3]
+k=4
+k=k%len(arr)
+def rev_arr(i,j):
+    while i<j:
+        temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+        i+=1
+        j-=1
+rev_arr(0,len(arr)-1)
+rev_arr(0,k-1)
+rev_arr(k,len(arr)-1)
+print(arr)
+#Output: [8, 1, 2, 3, 4, 5, 6, 7]
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # arr=[1,2,3,4,5,6,7,8]
 
 # n=3
@@ -21,31 +59,6 @@
 
 # res_arr=reverse(i,j,arr)
 # print(res_arr)
-
-
-
-
-
-
-arr=[4,5,6,7,8,1,2,3]
-k=4
-k=k%len(arr)
-def rev_arr(i,j):
-    while i<j:
-        temp=arr[i]
-        arr[i]=arr[j]
-        arr[j]=temp
-        i+=1
-        j-=1
-rev_arr(0,len(arr)-1)
-rev_arr(0,k-1)
-rev_arr(k,len(arr)-1)
-print(arr)
-#Output: [8, 1, 2, 3, 4, 5, 6, 7]
-    
-
-
-
         
     
 
