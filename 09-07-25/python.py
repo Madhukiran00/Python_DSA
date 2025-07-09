1f529959ecfa981b7afa7d55b3205c7b06cb06cf
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-07-09 19:55:54 +0530

    Update python.py

--- 09-07-25/python.py
+++ 09-07-25/python.py
@@ -23,14 +23,19 @@
 # print(res_arr)
 
 
-arr=[1,2,3,4,5,6,7,8]
+
+
+
+
+arr=[4,5,6,7,8,1,2,3]
+
 count=0
-i=0
-for j in range(1,len(arr)):
-    if arr[i]>arr[i+1]:
+
+for j in range(len(arr)-1):
+    if arr[j]>arr[j+1]:
         count+=1
         
-if arr[len(arr)-1]>arr[0]:
+if arr[len(arr)-1] > arr[0]:
     count+=1
 print(count==1)
         
