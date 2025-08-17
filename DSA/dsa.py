622ad137482775794e0c44fa8d7a4bfff1664a1c
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-08-17 16:33:23 +0530

    Update dsa.py

--- DSA/dsa.py
+++ DSA/dsa.py
@@ -1,5 +1,5 @@
 #list reverse
-lis=[8,7,6,5,4,3,2,1]
+'''lis=[8,7,6,5,4,3,2,1]
 i=0
 j=len(lis)-1
 while i<j:
@@ -8,11 +8,25 @@ while i<j:
     lis[j]=temp
     i+=1
     j-=1
-print(lis)
+print(lis)'''
 
 #---------------------------------
 # Leetcode =26
 
 # Remove duplicates from an sorted array
+
+'''lis=[1,2,2,3,4,4,4,6,7,7]
+i=0
+for j in range(1,len(lis)):
+    if lis[i]!=lis[j]:
+        lis[i+1]=lis[j]
+        i+=1
+print(lis[:i+1])'''
+
+#print the number of unique elements
+# print(i)
+#-------------------------------------
+
+
 
 
