a125b7cce6387d235c184762eaf8050e08d76733
Madhukiran00 <pillimadhukiran00@gmail.com>
2025-08-17 18:00:10 +0530

    Update dsa.py

--- DSA/dsa.py
+++ DSA/dsa.py
@@ -1,3 +1,35 @@
+# sec_max in a lis
+'''
+l=[3,5,6,7,83,5,7]
+
+max=0
+sec_max=0
+for i in range(len(l)):
+    if l[i]>max:
+        sec_max=max
+        max=l[i]
+    elif l[i]<max and l[i]>sec_max:
+        sec_max=l[i]
+print(sec_max)'''
+
+#----------------------------------
+# Kth max element 
+   
+'''l = [3, 5, 6, 7, 83, 5, 7]
+k = 3   # 3rd max
+unique = list(set(l))  # keep distinct elements
+
+max_list = []
+
+for _ in range(k):
+    m = max(unique)
+    max_list.append(m)
+    unique.remove(m)
+
+print(f"{k}th max:", max_list[-1])'''
+
+#---------------------------------
+
 #list reverse
 '''lis=[8,7,6,5,4,3,2,1]
 i=0
@@ -26,6 +58,28 @@ print(lis[:i+1])'''
 #print the number of unique elements
 # print(i)
 #-------------------------------------
+
+# Leetcode:- 1796. Second Largest Digit in a String
+
+'''s="sdgdf3hkk6lj9ljl34"
+
+max=-1
+sec_max=-1
+
+for i in range(len(s)):
+    if s[i].isdigit():
+        if int(s[i])>max:
+            sec_max=max
+            max=int(s[i])
+        if int(s[i])>sec_max and int(s[i])<max:
+            sec_max=int(s[i])'''
+            
+# print(sec_max)
+
+#-------------------------------------
+    
+
+
 
 
 
